302dc2539306b70547e3cef2c9808a22b1e397b0
Eric Van Wyk <evw@umn.edu>
2011-04-22 16:52:04 +0000

    moving v6 files into the host/extension/v6 and moving extensions/typeChecking to host/extensions

--- host/extensions/v6/Terminals.sv
+++ host/extensions/v6/Terminals.sv
@@ -1,5 +1,6 @@
 grammar edu:umn:cs:melt:ableP:host:core:terminals ;
 
+
 terminal FOR 'for' lexer classes {promela,promela_kwd};
 terminal IN  'in' ; -- lexer classes {promela,promela_kwd};
 terminal DOTDOT '..' ;
