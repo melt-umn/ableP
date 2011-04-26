6128431f141b189b9f8d17a1fc86365ff147eece
Eric Van Wyk <evw@umn.edu>
2011-04-26 11:04:37 +0000

    more cleaning up

---
+++ host/core/Core.sv
@@ -0,0 +1,5 @@
+grammar edu:umn:cs:melt:ableP:host:core ;
+
+exports edu:umn:cs:melt:ableP:host:core:concretesyntax ;
+exports edu:umn:cs:melt:ableP:host:core:abstractsyntax ;
+exports edu:umn:cs:melt:ableP:host:core:terminals ;
---
+++ host/extensions/Extensions.sv
@@ -0,0 +1,5 @@
+grammar edu:umn:cs:melt:ableP:host:extensions ;
+
+exports edu:umn:cs:melt:ableP:host:extensions:embeddedC ;
+exports edu:umn:cs:melt:ableP:host:extensions:v6 ;
+exports edu:umn:cs:melt:ableP:host:extensions:typeChecking ;
