52e92a13d1940cc994e0fad5492f20612efdd208
Eric Van Wyk <evw@umn.edu>
2011-05-20 08:18:39 +0000

    More terminal symbols for C embedding

---
+++ host/extensions/embeddedC/Terminals.sv
@@ -0,0 +1,9 @@
+grammar edu:umn:cs:melt:ableP:host:extensions:embeddedC ;
+
+import edu:umn:cs:melt:ableC:terminals ;
+
+terminal C_CODE  'c_code'   lexer classes {promela,promela_kwd};
+terminal C_DECL  'c_decl'   lexer classes {promela,promela_kwd};
+terminal C_EXPR  'c_expr'   lexer classes {promela,promela_kwd};
+terminal C_STATE 'c_state'  lexer classes {promela,promela_kwd};
+terminal C_TRACK 'c_track'  lexer classes {promela,promela_kwd};
