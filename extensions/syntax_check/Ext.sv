01779ed5a320c107fae0b89c005c577c6515d53d
Eric Van Wyk <evw@umn.edu>
2011-06-09 15:01:04 +0000

    new attempt at modular determinism analysis

---
+++ extensions/syntax_check/Ext.sv
@@ -0,0 +1,8 @@
+grammar edu:umn:cs:melt:ableP:extensions:syntax_check ;
+
+import edu:umn:cs:melt:ableP:host:core:concretesyntax ;
+
+parser extGrammar::Program_c { edu:umn:cs:melt:ableP:extensions:tables ; }
+
+-- Maybe the type of extGrammar should be Expr_c ?
+
--- extensions/syntax_check/ext/Ext.sv
+++
@@ -1,7 +0,0 @@
-grammar edu:umn:cs:melt:ableP:extensions:syntax_check:ext ;
-
-import edu:umn:cs:melt:ableP:concretesyntax ;
-
-parser pp::Expr_c { edu:umn:cs:melt:ableP:extensions:tables ; }
-
-
