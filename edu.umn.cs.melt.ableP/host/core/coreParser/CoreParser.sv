3625ea8c50bb47b72507e9bb9618268f0a7ac6e2
Lucas Kramer <krame505@umn.edu>
2019-12-10 13:24:01 -0600

    Update comment about comments

--- edu.umn.cs.melt.ableP/host/core/coreParser/CoreParser.sv
+++ edu.umn.cs.melt.ableP/host/core/coreParser/CoreParser.sv
@@ -8,7 +8,7 @@ parser promelaCoreParser :: Program_c {
  edu:umn:cs:melt:ableP:host:core:concretesyntax ;
 
  -- We need to import the ableC terminals since they define a lexer class,
- -- Ccomment, that Promela coments and white space take precedence over.
+ -- Comment, that Promela coments and white space take precedence over.
  -- Without including this, Copper raises an error.
  -- ToDo: file a bug about this and put bug# here.
  edu:umn:cs:melt:ableC:concretesyntax ;
